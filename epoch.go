66d09312d28f24ec4e41adb016531fe201d09525
howard <howard@howard-6a8e-zr709.local>
2022-06-15 11:48:00 +0800

    add some examples

---
+++ epoch.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	p := fmt.Println
+	p(now)
+
+	p(now.Unix())
+	p(now.UnixMilli())
+	p(now.UnixNano())
+
+	p(time.Unix(now.Unix(), 0))
+	p(time.Unix(0, now.UnixNano()))
+}
